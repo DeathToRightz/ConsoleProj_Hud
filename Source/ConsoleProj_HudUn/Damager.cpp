8296abb60dd7ed23a2a0bde796058a7f589cf077
Logan Cordova <logan.cordova@gmail.com>
2025-10-15 14:44:15 -0400

    Add Damage Function

---
+++ Source/ConsoleProj_HudUn/Damager.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Kismet/GameplayStatics.h"
+#include "Damager.h"
+
+// Sets default values
+ADamager::ADamager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("HitBox"));
+	BoxCollider->SetBoxExtent(FVector(30.f, 30.f, 30.f));
+	BoxCollider->SetCollisionProfileName("BlockAllDynamic");
+	RootComponent = BoxCollider;
+
+	BoxCollider->OnComponentHit.AddDynamic(this, &ADamager::OnComponentHit);
+}
+
+// Called when the game starts or when spawned
+void ADamager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADamager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADamager::OnComponentHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("We hit something"));
+
+	
+
+		UGameplayStatics::ApplyDamage(OtherActor, 1.f, GetWorld()->GetFirstPlayerController(), this, UDamageType::StaticClass());
+	
+	
+
+}
+
---
+++ Source/ConsoleProj_HudUn/HealthComponent.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+
+	Owner = GetOwner(); //Get our parent the actor/Player
+
+	if (Owner)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Actor connected"));
+
+		Owner->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage); //Bind the take damage method to the player
+	}
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UHealthComponent::TakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigateBy, AActor* DamageCauser)
+{
+	//Handle taking damage
+
+	UE_LOG(LogTemp, Warning, TEXT("Actor Took Damage"));
+}
+
